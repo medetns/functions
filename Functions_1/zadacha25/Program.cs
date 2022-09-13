76b65bef4e78615e9a13cb2716b481abd7f981c6
Medet <medetnurm@gmail.com>
2022-09-13 20:01:24 +0600

    zadacha25

---
+++ Functions_1/zadacha25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+
+int PowerofA(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        result = result * numA;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите A: ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите B: ");
+int numB = int.Parse(Console.ReadLine());
+
+int powerofnumber = PowerofA(numA, numB);
+Console.WriteLine(powerofnumber);
