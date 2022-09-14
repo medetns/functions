4061cbe18dbc7fdc98db461b21fbdaacabb38969
Medet <medetnurm@gmail.com>
2022-09-14 10:07:35 +0600

    zadacha29

---
+++ Functions_1/zadacha29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//массив с рандомными числами
+int[] FillArray(int[] filling)
+{
+for (int i = 0; i < filling.Length; i++)
+{
+filling[i] = new Random().Next();
+
+}
+return filling;
+}
+
+void printArray(int[] writing)
+{
+for (int i = 0; i < writing.Length; i++)
+{
+Console.Write(writing[i] + " ");
+}
+}
+
+int[] myArray = new int[8];
+
+FillArray(myArray);
+printArray(myArray);
