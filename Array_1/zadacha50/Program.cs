23bafbfd45c3cd91d8fd0a23e01dd877496df1b7
Medet <medetnurm@gmail.com>
2022-09-22 12:55:18 +0600

    zadacha50

---
+++ Array_1/zadacha50/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] res = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            res[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return res;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindNumber(int[,] array)
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    bool flag = false;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == number)
+            {
+                flag = true;
+            }
+        }
+    }
+    if (flag == true)
+    {
+        Console.WriteLine(number);
+    }
+    else
+    {
+        Console.WriteLine($"{number} -> такого числа в массиве нет");
+    }
+}
+
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, -100, 100);
+PrintArray(array);
+FindNumber(array);
