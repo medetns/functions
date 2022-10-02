384d1a086e567f14af1aff24ca2d5546e7049efe
Medet <medetnurm@gmail.com>
2022-10-02 22:47:17 +0600

    zadacha62

---
+++ Array_2/zadacha62/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+
+Console.Clear();
+Console.WriteLine($"Задача 62: Заполните спирально массив 4 на 4.");
+
+int n = 4;
+int[,] spiralArray = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= spiralArray.GetLength(0) * spiralArray.GetLength(1))
+{
+  spiralArray[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < spiralArray.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= spiralArray.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > spiralArray.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+PrintArray(spiralArray);
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
