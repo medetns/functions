289e78aa1b8840efda95d8a214f6213f276a1647
Medet <medetnurm@gmail.com>
2022-10-06 14:00:48 +0600

    zadacha66

---
+++ Recursion/zadacha66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int PrintNumbers(int start, int end)
+{
+    int sum = 0;
+    if (start == end + 1) return sum;
+    return (sum + start + PrintNumbers(start + 1, end));
+}
+
+Console.WriteLine(PrintNumbers(firstNumber, secondNumber));
